cf96ba5f8d16e20d4e1f238906af8665c98445f3
bahatiphill <gur79tub@gmail.com>
2022-01-26 21:54:58 +0200

    Beta-1

--- simple_blockchain.py
+++ simple_blockchain.py
@@ -37,13 +37,13 @@ class Blockchain:
         self.transactions = []
 
         # Initialize the genesis block
-        #genesis_block_hash =  self.hash_theblock(b"genesis")
         self.create_block(id=0, transactions=[])
         
 
     def create_block(self, id, transactions, prevblock_hash=""):
         time_stamp = time.time()
         new_block = Block(id, transactions,time_stamp,prevblock_hash, None)
+
         #hashing the block
         new_block_dict = dataclasses.asdict(new_block)
         encoded_block_data = json.dumps(new_block_dict).encode()
@@ -72,10 +72,9 @@ class Blockchain:
         return hashlib.sha256(data).hexdigest()
 
 
-    # NOT REQUIRED BY HW
+    # NOT REQUIRED BY HW-1
     def create_account(self, coins=100):
         address = '0b'+''.join(random.choices(string.ascii_letters, k=5))
-        #new_account = {"address": address, "coins":coins}
         new_account = Account(address, coins)
         self.accounts.append(new_account)
         return new_account
@@ -83,34 +82,91 @@ class Blockchain:
 
 def main():
 
-    ##STEP-1: Initialize the blockchain
-    blockchain = Blockchain()
+    #Decoration
+    print("")
+    print("########################################################")
+    print("############# THIS IS A SIMPLE BLOCKCHAIN ##############")
+    print("#############@@ Author: Philbert Bahati @@##############")
+    print("########################################################")
+    print("")
 
-    ##STEP-2: Create Accounts(users) on the block | at least 3.
+    ##STEP-1: Initialize the blockchain
+    print(">>Creating the blockchain......")
+    time.sleep(1)
+    print(">>Generating the GENESIS block......")
+    print("")
+    blockchain = Blockchain()
+    time.sleep(1)
+    print("DONE....")
+    print("")
+
+    ##step-2: Get the last Block. For this time it should be the genesis block
+    print("Let's get the latest block infomation for now, ")
+    print("Which is the Genesis block for now")
+    print("")
+    last_block = blockchain.get_last_block()
+    print("---------------------------------------------------------------")
+    print("The last Block ID: ", last_block.id)
+    print("The last block hash Value : ", last_block.block_hash)
+    print("The last block Timestamp : ", last_block.timestamp)
+    print("The last block List of transactions : ", last_block.transactions)
+    print("---------------------------------------------------------------")
+    print("")
+
+
+
+    ##STEP-3: Create Accounts(users) on the block | at least 3.
+    time.sleep(2)
+    print("Let's create 3 addresses(users) in the blockchain")
+    print("DONE...")
+    time.sleep(1)
+    print("")
     alice_ = blockchain.create_account(150) # Alice accounnt
     bob_ = blockchain.create_account(750) # Bob accounnt
     zeke_ = blockchain.create_account(80)  # Zeke accounnt
+    
+    print(">> Address-1 (Let's call her Alice) :", alice_.address)
+    print(">> Address-2 (Let's call her Bob) :", bob_.address)
+    print(">> Address-3 (Let's call her Zeke) :", zeke_.address)
+    print("")
 
-    ##step-2.5: Get the last Block. For this time it should be the genesis block
-    last_block = blockchain.get_last_block()
-    print("The last Block ID: ", last_block.id)
-    print("The last block hash Value : ", last_block.block_hash)
 
-    ##STEP-3: Initiate  transactions| at least 2.
+    ##STEP-4: Initiate  transactions| at least 2.
+    print("Let's create some transaction in the blockchain")
+    time.sleep(1)
     transaction_1 = blockchain.create_transaction(0, 10, alice_.address, bob_.address)  #From Alice to Bob
     transaction_2 = blockchain.create_transaction(1, 25,bob_.address,zeke_.address)  #From Bob to zeke
+    print(">> TRANSACTION-1: Sending.. ", transaction_1["amount"], "Coins.  [FROM]:",transaction_1["from"], " [TO]:", transaction_1["to"])
+    time.sleep(0.5)
+    print(">> TRANSACTION-2: Sending.. ", transaction_2["amount"], "Coins.  [FROM]:",transaction_2["from"], " [TO]:", transaction_2["to"])
+    time.sleep(0.5)
+    print(">> Adding Transactions to the block...")
 
 
-    ##STEP-4: Create a block. Add those 2 transaction in a block.
+    ##STEP-5: Create a block. Add those 2 transaction in a block.
     prev_hash = blockchain.get_last_block()
     prev_hash = prev_hash.block_hash
     blockchain.create_block(1, [transaction_1, transaction_2], prev_hash)
-    
-    ##STEP-7: Get the last block.
-    last_block = blockchain.get_last_block()
+    print("DONE...")
     print("")
+
+    ##STEP-6: Get the last block.
+    print("Let's get the latest block infomation Again, ")
+    print("It should include the latest transactions..")
+    print("")
+    time.sleep(1)
+
+
+    last_block = blockchain.get_last_block()
+    print("---------------------------------------------------------------")
     print("The last Block ID: ", last_block.id)
     print("The last block hash Value : ", last_block.block_hash)
+    print("The last block Timestamp : ", last_block.timestamp)
+    print("The last block List of transactions : ", last_block.transactions)
+    print("---------------------------------------------------------------")
+    print("")
+    print("DONE & BYE")
+
 
 
 
